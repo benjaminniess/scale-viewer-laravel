80db76aa7d698f1e5c8560c00596d576b4582623
Benjamin Niess <benjamin.niess@gmail.com>
2021-09-30 16:54:20 +0200

    Removed previous web routes and redirect FP to api boards list

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreBoard;
-use App\Http\Requests\UpdateBoard;
 use App\Models\Board;
-use Auth;
 
 class BoardsController extends Controller {
 
@@ -17,72 +14,12 @@ class BoardsController extends Controller {
 		return Board::all();
 	}
 
-
+	/**
+	 * @param int $boardID
+	 *
+	 * @return Board
+	 */
 	public function show( int $boardID ) {
 		return Board::find( $boardID );
-	}
-
-	/**
-	 * Show the form for creating a new board.
-	 *
-	 * @return \Illuminate\Contracts\View\Factory
-	 */
-	public function create() {
-		return view( 'boards.create', [
-			'templates' => \App\Models\Board::$templates,
-		] );
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param \App\Http\Requests\StoreBoard $request
-	 *
-	 * @return \Illuminate\Http\RedirectResponse
-	 */
-	public function store( StoreBoard $request ) {
-		$board = Board::create( [
-			'title'       => request( 'title' ),
-			'description' => request( 'description' ),
-			'author_id'   => auth()->user()->id,
-			'status'      => 'new',
-			'template'    => request( 'template' ),
-		] );
-
-		return redirect( route( 'show_board', $board->id ) );
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param Board $board
-	 *
-	 * @return \Illuminate\Contracts\View\Factory
-	 */
-	public function edit( Board $board ) {
-		$this->authorize( 'update', $board );
-
-		return view( 'boards.edit', [
-			'board'          => $board,
-			'back_permalink' => route( 'show_board', $board->id ),
-			'numbers'        => $board->numbers()->get(),
-			'template'       => $board->template,
-			'templates'      => \App\Models\Board::$templates,
-		] );
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param \App\Http\Requests\UpdateBoard $request
-	 * @param \App\Models\Board $board
-	 *
-	 * @return \Illuminate\Http\RedirectResponse
-	 */
-	public function update( UpdateBoard $request, Board $board ) {
-		$this->authorize( 'update', $board );
-		$board->update_board( $request );
-
-		return redirect( route( 'show_board', $board->id ) );
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,37 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/', function () {
-	$boards = \App\Models\Board::all();
-
-	return view( 'welcome', [
-		'boards' => $boards,
-	] );
-} )->name( 'home' );
-
-Route::get( '/board/create', 'BoardsController@create' )
-     ->middleware( 'auth' )
-     ->name( 'create_board' );
-
-Route::get( '/board/{board}/edit', 'BoardsController@edit' )
-     ->middleware( 'auth' )
-     ->name( 'edit_board' );
-
-Route::post( '/board', 'BoardsController@store' )
-     ->middleware( 'auth' )
-     ->name( 'store_board' );
-Route::put( '/board/{board}', 'BoardsController@update' )
-     ->middleware( 'auth' )
-     ->name( 'update_board' );
-
-Route::post( '/board/{board}/numbers', 'NumbersController@store' )
-     ->middleware( 'auth' )
-     ->name( 'store_number' );
-Route::get( '/board/{board}/numbers/{number}/edit', 'NumbersController@edit' )
-     ->middleware( 'auth' )
-     ->name( 'edit_number' );
-Route::put( '/board/{board}/numbers/{number}', 'NumbersController@update' )
-     ->middleware( 'auth' )
-     ->name( 'update_number' );
-
-Auth::routes();
+Route::redirect( '/', '/api/boards' );
