006496a602fadb03f74072a0d177a7030709b555
Benjamin Niess <benjamin.niess@gmail.com>
2021-10-01 11:29:16 +0200

    Updated user factory based on previous version of laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,43 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Board;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Authenticatable {
+	use Notifiable;
+	use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [ 'name', 'email', 'password' ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [ 'password', 'remember_token' ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    /**
-     * @param \App\Models\Board $board
-     * @return bool
-     */
-    public function is_author(Board $board)
-    {
-        return (int) $this->id === (int) $board->author_id;
-    }
+	/**
+	 * @param \App\Models\Board $board
+	 *
+	 * @return bool
+	 */
+	public function is_author( Board $board ) {
+		return (int) $this->id === (int) $board->author_id;
+	}
 }
