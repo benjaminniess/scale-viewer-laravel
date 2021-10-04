57c3f3468ba6b815af631410aa44feb6142a821b
Benjamin Niess <benjamin.niess@gmail.com>
2021-10-04 17:41:41 +0200

    Removed old blade templates

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,47 +6,33 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+class LoginController extends Controller {
+	/*
+	|--------------------------------------------------------------------------
+	| Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles authenticating users for the application and
+	| redirecting them to your home screen. The controller uses a trait
+	| to conveniently provide its functionality to your applications.
+	|
+	*/
 
-    use AuthenticatesUsers;
+	use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-
-    /**
-     * Override the default login controller so we can pass some data
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function showLoginForm() {
-        return view('auth.login', [
-            'api_key' => config('firebase.api_key'),
-            'app_name' => config('firebase.app_name'),
-        ]);
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware( 'guest' )->except( 'logout' );
+	}
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,59 @@
 @extends('layouts.default')
 
-@section('head')
-<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-auth.js"></script>
-<script>
-    var firebaseConfig = {
-        apiKey: "{{ $api_key }}",
-        authDomain: "{{ $app_name }}.firebaseapp.com",
-        databaseURL: "https://{{ $app_name }}.firebaseio.com",
-    };
-</script>
-@endsection
 
 @section('content')
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-    <div class="field">
-        <label class="label" for="email">Email</label>
-        <div class="control">
-            <input  id="email" type="email" class="input" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autofocus required>
-        </div>
-
-        @error('email')
-            <p class="help is-danger">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div class="field">
-        <label class="label" for="password">Password</label>
-        <div class="control">
-            <input id="password" type="password" class="input @error('password') is-invalid @enderror" name="password" required>
-        </div>
-
-        @error('password')
-        <p class="help is-danger">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div class="field">
-        <div class="control">
-            <label class="checkbox">
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                Remember me
-            </label>
-        </div>
-    </div>
-
-    <div class="field is-grouped">
-        <div class="control">
-            <button type="submit" class="button is-link">Login</button>
-        </div>
-
-        <div class="control">
-            <button type="button" id="google-connect-button" class="button is-link">Login with Google</button>
-        </div>
-
-        @if (Route::has('password.request'))
+        <div class="field">
+            <label class="label" for="email">Email</label>
             <div class="control">
-                <a href="{{ route('password.request') }}" class="button is-light">Forgot your password?</a>
+                <input id="email" type="email" class="input" @error('email') is-invalid @enderror" name="email"
+                value="{{ old('email') }}" autofocus required>
             </div>
-        @endif
-    </div>
-</form>
+
+            @error('email')
+            <p class="help is-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="field">
+            <label class="label" for="password">Password</label>
+            <div class="control">
+                <input id="password" type="password" class="input @error('password') is-invalid @enderror"
+                       name="password" required>
+            </div>
+
+            @error('password')
+            <p class="help is-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox">
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                    Remember me
+                </label>
+            </div>
+        </div>
+
+        <div class="field is-grouped">
+            <div class="control">
+                <button type="submit" class="button is-link">Login</button>
+            </div>
+
+            <div class="control">
+                <button type="button" id="google-connect-button" class="button is-link">Login with Google</button>
+            </div>
+
+            @if (Route::has('password.request'))
+                <div class="control">
+                    <a href="{{ route('password.request') }}" class="button is-light">Forgot your password?</a>
+                </div>
+            @endif
+        </div>
+    </form>
 
 @endsection
